ce34d7e644db270a9147f2068809d9e12d630ac0
Tyler Veness <calcmogul@gmail.com>
2016-09-21 18:38:04 -0700

    bootstrap: added link to std_lib_facilities.h
    Change-Id: I16b670484e7931d110b7655fc5ad843d3462c393

---
+++ bootstrap/std_lib_facilities.h
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+inline void keep_window_open() {
+  char ch;
+  cin >> ch;
+}
