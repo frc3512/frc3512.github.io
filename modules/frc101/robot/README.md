e501783282f972b22f3e26cb3e73279dc40174bf
Tyler Veness <calcmogul@gmail.com>
2017-01-08 01:50:45 -0800

    modules: frc101: added generic and robot code project setup instructions
    Change-Id: I5cc3372429ec8bcfa6520b5ecdb3d9c197eb52da

---
+++ modules/frc101/robot/README.md
@@ -0,0 +1,3 @@
+# FRC Team 3512's 2017 Robot
+
+The source code for our 2017 FRC robot named ?.
---
+++ modules/frc101/robot/src/Constants.hpp
@@ -0,0 +1,5 @@
+// Copyright (c) FRC Team 3512, Spartatroniks 2017. All Rights Reserved.
+
+#pragma once
+
+// Includes definition for Talons and etc that connect to the RoboRIO
