45e6e266d20fe03eb1a2326d400e5fcf6351e112
Tyler Veness <calcmogul@gmail.com>
2017-03-01 17:31:13 -0800

    Fixed id attribute generation for header tags containing <a> tags
    Change-Id: I454f4e99d9be23d3b33cd0e1884f04c60656237b

--- gen_id_attributes.py
+++ gen_id_attributes.py
@@ -9,27 +9,38 @@ def main():
         os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser("."))
         if "MathJax" not in dp for f in fn if f.endswith(".html")
     ]
-    line_regex = re.compile("(?P<start>\s*<h[2-6])"
-                            ".*>"
+    line_regex = re.compile("(?P<open>\s*<h[2-6] )"
+                            "(?P<id>.*id=\"[A-Za-z0-9\-._]*\")?"
+                            "(?P<close>>)"
                             "(?P<content>.*)"
-                            "(?P<end></h[2-6]>)")
-    char_regex = re.compile("[^a-zA-Z0-9\-._]")
+                            "(?P<end></h[2-6]>.*)")
+    char_regex = re.compile("[^A-Za-z0-9\-._]")
     for name in files:
         lines = ""
         modified = False
+        in_tag = False
         with open(name, "r") as input:
             for line in input:
                 match = line_regex.search(line)
                 if match:
-                    edit = match["start"] + " id=\""
+                    edit = match["open"] + "id=\""
                     for char in match["content"]:
-                        if char == " ":
-                            edit += "_"
-                        elif char_regex.match(char) and char != "\n":
-                            edit += "." + format(ord(char), "X")
-                        else:
-                            edit += char
-                    edit += "\">" + match["content"] + match["end"] + "\n"
+                        if char == "<":
+                            in_tag = True
+                            continue
+                        elif char == ">":
+                            in_tag = False
+                            continue
+
+                        if not in_tag:
+                            if char == " ":
+                                edit += "_"
+                            elif char_regex.match(char) and char != "\n":
+                                edit += "." + format(ord(char), "X")
+                            else:
+                                edit += char
+                    edit += "\"" + match["close"] + match["content"] + match[
+                        "end"] + "\n"
 
                     if line != edit:
                         modified = True
