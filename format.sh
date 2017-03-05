0d7545e5a133e3756fc98886d5ea0eec2e78c5e9
Tyler Veness <calcmogul@gmail.com>
2017-03-04 21:25:55 -0800

    format.sh now skips Angelscript Doxygen files
    Change-Id: Ide155625c7ecea6905afb3fdda9de0b1796ac3df

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find . -type f -name \*\.html ! -path "./MathJax/*" ! -name "google*.html" -exec tidy -i -m -q -w 80 -ashtml -utf8 {} \;
-find . -type f -name \*\.html ! -path "./MathJax/*" ! -name "google*.html" -exec sed -i '/<meta name="generator"/d' {} \;
+find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec tidy -i -m -q -w 80 -ashtml -utf8 {} \;
+find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec sed -i '/<meta name="generator"/d' {} \;
