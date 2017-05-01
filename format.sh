a5620e3e35e37c836e86f998e5bc0f3875ac6546
Tyler Veness <calcmogul@gmail.com>
2017-05-01 04:36:22 -0700

    Rewrote Git presentation in reveal.js
    Change-Id: Idbb138c850165344e3280a78c62d3e8ea10d7092

--- format.sh
+++ format.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec tidy -i -m -q -w 80 -ashtml -utf8 {} \;
-find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec sed -i '/<meta name="generator"/d' {} \;
+find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./reveal.js/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec tidy -i -m -q -w 80 -ashtml -utf8 {} \;
+find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./reveal.js/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec sed -i '/<meta name="generator"/d' {} \;
---
+++ reveal-init.js
@@ -0,0 +1,8 @@
+Reveal.initialize({
+  dependencies: [
+    { src: '../../../reveal.js/plugin/markdown/marked.js' },
+    { src: '../../../reveal.js/plugin/markdown/markdown.js' },
+    { src: '../../../reveal.js/plugin/notes/notes.js', async: true },
+    { src: '../../../reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
+  ]
+});
