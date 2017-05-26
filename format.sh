7a6bc93946d2d3201169260bc8c8166a8c4d8c60
Tyler Veness <calcmogul@gmail.com>
2017-05-25 20:20:42 -0700

    Added check for tidy-html5 to format.sh
    Change-Id: Ia41d75188cf8f5e15829d30ca74ab570f0db94b0

--- format.sh
+++ format.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./reveal.js/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec tidy -i -m -q -w 80 -ashtml -utf8 {} \;
-find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./reveal.js/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec sed -i '/<meta name="generator"/d' {} \;
+if tidy -v &> /dev/null; then
+  find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./reveal.js/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec tidy -i -m -q -w 80 -ashtml -utf8 {} \;
+  find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./reveal.js/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec sed -i '/<meta name="generator"/d' {} \;
+else
+  echo ${0##*/}: tidy-html5 not found
+fi
