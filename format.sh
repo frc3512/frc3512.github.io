6b77ac98259b418e9901060c13324b833d4c5a74
Tyler Veness <calcmogul@gmail.com>
2017-07-02 00:59:51 -0400

    tidy no longer adds meta tag
    Change-Id: I74a97e7cdc857b0876b8cfb571075c23c19b163d

--- format.sh
+++ format.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if tidy -v &> /dev/null; then
-  find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./reveal.js/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec tidy -i -m -q -w 80 -ashtml -utf8 {} \;
-  find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./reveal.js/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec sed -i '/<meta name="generator"/d' {} \;
+  find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./reveal.js/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec tidy -i -m -q -w 80 --tidy-mark false -ashtml -utf8 {} \;
 else
   echo ${0##*/}: tidy-html5 not found
 fi
