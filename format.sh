ce37329e6635bc2995a94cd628ac39969ade0259
Tyler Veness <calcmogul@gmail.com>
2017-07-07 01:20:15 -0400

    tidy flags moved to .conf file
    Change-Id: I3bf0fcbc48d7625836d7ca3b0b34b5a96f937d87

--- format.sh
+++ format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if tidy -v &> /dev/null; then
-  find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./reveal.js/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec tidy -i -m -q -w 80 --tidy-mark false -ashtml -utf8 {} \;
+  find . -type f -name \*\.html ! -path "./MathJax/*" ! -path "./reveal.js/*" ! -path "./archives/angelscript/docs/*" ! -name "google*.html" -exec tidy -config html-tidy.conf -modify -quiet --tidy-mark false {} \;
 else
   echo ${0##*/}: tidy-html5 not found
 fi
