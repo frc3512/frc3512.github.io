1e425d374d457b235387b4066774fb03df8e9cdc
Tyler Veness <calcmogul@gmail.com>
2020-08-20 10:57:24 -0700

    Bump reveal.js version to 4.0.2

--- reveal-init.js
+++ reveal-init.js
@@ -1,10 +1,5 @@
 Reveal.initialize({
   center: false,
   transition: 'none',
-  dependencies: [
-    { src: '../../../reveal.js/plugin/markdown/marked.js' },
-    { src: '../../../reveal.js/plugin/markdown/markdown.js' },
-    { src: '../../../reveal.js/plugin/notes/notes.js', async: true },
-    { src: '../../../reveal.js/plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } }
-  ]
+  plugins: [ RevealNotes, RevealMarkdown, RevealHighlight ]
 });
